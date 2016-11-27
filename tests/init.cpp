1d518f87a789661e4d706b4b56050b249b7c8b66
Arteezy09 <arteezybezskilla@mail.ru>
2016-11-27 20:51:09 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -5,7 +5,7 @@
  
 SCENARIO("count and get for empty shared", "[count&get]"){
  shared_ptr<size_t> s;
-  REQUIRE(s.count()==0);
+  REQUIRE(s.refs()==0);
  REQUIRE(s.get()==nullptr);
 }
 
@@ -13,34 +13,35 @@ SCENARIO("get for shared initialize", "[getInShared]"){
  shared_ptr<size_t> s(new size_t[10]);
  REQUIRE(s.get()!=nullptr);
 }
+
 SCENARIO("Initialize Shared", "[InShGet]") {
 	size_t * ptr = new size_t[10];
 	shared_ptr<size_t> s(ptr);
-
 	REQUIRE(ptr == s.get());
 }
+
 SCENARIO("Checking count", "[CheckCount]") {
 	size_t * ptr = new size_t[10];
 	shared_ptr<size_t> f_sp(ptr);
 
-	REQUIRE(f_sp.count() == 1);
+	REQUIRE(f_sp.refs() == 1);
 
 	shared_ptr<size_t> s_sp(f_sp);
 
-	REQUIRE(f_sp.count() == 2);
-	REQUIRE(s_sp.count() == 2);
+	REQUIRE(f_sp.refs() == 2);
+	REQUIRE(s_sp.refs() == 2);
 
 	shared_ptr<size_t> t_sp(s_sp);
 
-	REQUIRE(f_sp.count() == 3);
-	REQUIRE(s_sp.count() == 3);
-	REQUIRE(t_sp.count() == 3);
+	REQUIRE(f_sp.refs() == 3);
+	REQUIRE(s_sp.refs() == 3);
+	REQUIRE(t_sp.refs() == 3);
 }
 SCENARIO("My","[MY]"){
 
 shared_ptr<size_t> s;
 shared_ptr<size_t> s2(s);
-REQUIRE(s2.count()==0);
+REQUIRE(s2.refs()==0);
 }
 
 
